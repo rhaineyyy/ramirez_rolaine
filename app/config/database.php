3d8edb88c20e7a819ce454daec48b5bcd330310e
unknown <rolaineramirez0623@gmail.com>
2025-09-13 22:11:16 +0800

    first comitt

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'mockdata',
+    'username'	=> 'sql12798299',
+    'password'	=> 'crCZ1ygVHp',
+    'database'	=> 'sql12798299',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
