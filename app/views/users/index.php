66b6bf9a0912f5677d881b6056d146567fe1852f
unknown <rolaineramirez0623@gmail.com>
2025-10-05 11:08:24 +0800

    r

--- app/views/users/index.php
+++ app/views/users/index.php
@@ -913,7 +913,7 @@
           <?php endif; ?>
           <th>Action</th>
         </tr>
-        <?php foreach ($users as $user): ?>
+        <?php foreach ($user as $user): ?>
         <tr>
           <td><?=html_escape($user['id']); ?></td>
           <td><?=html_escape($user['username']); ?></td>
